56cd88e5c95c5ea80c21366448b331f5df6f2687
yaoleib <yaoleib@yonyou.com>
2017-11-30 21:31:20 +0800

    简洁工程

--- js/main.js
+++
@@ -1,4 +0,0 @@
-//here is your code...
-summerready = function () {
-	$summer.byId("content").innerHTML += "<h3 style='text-align: center'>Hello friends, welcome to touch the summer frame!</h3><h3 style='text-align: center'>The frame update at " +(new Date()).toLocaleString()+"</h3>";
-};
