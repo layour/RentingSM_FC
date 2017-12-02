d0c9c5fe5a4e8bc1ffc93ae422c4d228246a0e70
levo <zhangtao13140725@163.com>
2017-12-02 19:06:32 +0800

    添加车辆 选择经销商页面

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 ﻿
 
 summerready = function(){
-    // here is your code...	
+    // here is your code...
     var top = $summer.offset($summer.byId('header')).h;
 	var bottom = $summer.offset($summer.byId('footer')).h;
     summer.openFrame({
