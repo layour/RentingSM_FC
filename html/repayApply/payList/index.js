4169e87442e21eb993c55466172fca97558c0018
Sfl <942368681@qq.com>
2017-12-03 13:12:23 +0800

    sfl

---
+++ html/repayApply/payList/index.js
@@ -0,0 +1,54 @@
+summerready = function() {
+   var top = $('.um-header').height() + $('.navbar').height();
+   console.log(top);
+   summer.openFrameGroup({
+      id: 'group1',
+      background: '#ffffff',
+      scrollEnabled: false,
+      position: {
+         top: top,
+         bottom: 0,
+         left: 0,
+         right: 0
+      },
+      index: 0,
+      frames: [
+         {
+            id: 'all',
+            url: 'html/repayApply/payList/all.html',
+            bgColor: '#ffffff',
+            hidden: false
+         }, {
+            id: 'toBeSubmitted',
+            url: 'html/repayApply/payList/toBeSubmitted.html',
+            bgColor: '#ffffff',
+            hidden: true
+         }, {
+            id: 'loaning',
+            url: 'html/repayApply/payList/loaning.html',
+            bgColor: '#ffffff',
+            hidden: true
+         }, {
+            id: 'loaned',
+            url: 'html/repayApply/payList/loaned.html',
+            bgColor: '#ffffff',
+            hidden: true
+         }, {
+            id: 'notPass',
+            url: 'html/repayApply/payList/notPass.html',
+            bgColor: '#ffffff',
+            hidden: true
+         }
+      ]
+   }, function(ret, err) {
+      var index = ret.index;
+   });
+}
+$(function() {
+   $('.pay_list li').on('click', function() {
+      var tar = $(this).index().toString();
+      $('.pay_list li').removeClass('active');
+      $(this).addClass('active');
+      summer.setFrameGroupAttr({id: 'group1', index: tar});
+   });
+});
