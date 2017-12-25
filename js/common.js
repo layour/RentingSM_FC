0f1b8c12201b870f7a6598ccc476a349cdcd537e
zhouleim@yonyou.com <zhouleim@yonyou.com>
2017-12-25 20:08:59 +0800

    修改登录信息，common.js

--- js/common.js
+++ js/common.js
@@ -94,7 +94,7 @@ function ajaxRequest(paramObj, successCallback, errorCallback) {
 	};
 	if (userId()) {
 		paramData = paramObj.param;
-		paramData.EMPLOYEE_ID ="11357";
+		paramData.EMPLOYEE_ID = userId();
 	} else {
 		paramData = paramObj.param;
 	}
